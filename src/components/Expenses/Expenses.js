39829e4c49f68750bd4f791f9062de514ec45c25
TenguTerror <vegasduard@gmail.com>
2023-10-11 19:15:20 -0600

    filter year is now working in expenses component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,6 +11,10 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <div>
       <Card className='expenses'>
@@ -18,7 +22,7 @@ const Expenses = (props) => {
           value={filteredYear}
           onChangeFilter={filterChangedHandler}
         />
-        {props.items.map((expense) => (
+        {filteredExpenses.map((expense) => (
           <ExpenseItem
             key={expense.id}
             title={expense.title}
