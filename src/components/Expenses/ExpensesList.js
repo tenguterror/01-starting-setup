3dce192e9d230ddfb6ae07936b5fa39de4a32a25
TenguTerror <vegasduard@gmail.com>
2023-10-15 19:03:34 -0600

    moved the filter from expenses to own component

---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import ExpenseItem from './ExpenseItem';
+
+const ExpensesList = (props) => {
+  let expensesContent = <p>No expense found.</p>;
+
+  if (props.items.length > 0) {
+    expensesContent = props.items.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+};
+
+export default ExpensesList;
