d5ae0f8c5d4bce0dcf9658af67868d645e2c66c6
TenguTerror <vegasduard@gmail.com>
2023-10-16 18:54:53 -0600

    clean up expenseForm, start new window condition

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -6,37 +6,17 @@ const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState('');
   const [enteredAmount, setEnteredAmount] = useState('');
   const [enteredDate, setEnteredDate] = useState('');
-  // const [userInput, setUserInput] = useState({
-  //   enteredTitle: '',
-  //   enteredAmount: '',
-  //   enteredDate: '',
-  // });
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
-    // setUserInput({
-    //   ...userInput,
-    //   enteredTitle: event.target.value,
-    // });
-    // setUserInput((prevState) => {
-    //   return { ...prevState, enteredTitle: event.target.value };
-    // });
   };
 
   const amountChangeHandler = (event) => {
     setEnteredAmount(event.target.value);
-    // setUserInput({
-    //   ...userInput,
-    //   enteredAmount: event.target.value,
-    // });
   };
 
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
-    // setUserInput({
-    //   ...userInput,
-    //   enteredDate: event.target.value,
-    // });
   };
 
   const submitHandler = (event) => {
@@ -54,9 +34,12 @@ const ExpenseForm = (props) => {
     setEnteredDate('');
   };
 
+  let window = <button>Add New Expense</button>;
+
   return (
     <form onSubmit={submitHandler}>
-      <div className='new-expense__controls'>
+      {window}
+      {/* <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
           <input
@@ -88,7 +71,7 @@ const ExpenseForm = (props) => {
       </div>
       <div className='new-expense__actions'>
         <button type='submit'>Add expense</button>
-      </div>
+      </div> */}
     </form>
   );
 };
